6807c302bbdda44f0929165d5788dde0cea1c6e1
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2021-12-09 13:55:07 +0600

    font awsome css link need to be update

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css';
+import Rating from 'react-rating';
 
 
 const Product = (props) => {
     //console.log('clicked')
 
-    const {name,img,price,stock,seller}=props.product;
+    const {name,img,price,stock,seller,star}=props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         
@@ -27,6 +29,14 @@ const Product = (props) => {
             <p>Price:$ {price}</p>
    
             <p><small>only {stock} left in stock order soon</small></p>
+            <Rating 
+
+             initialRating={star}
+             emptySymbol="far fa-star"
+             fullSymbol="fas fa-star"
+             
+            readonly> </Rating>
+            <br />
              <button onClick={()=>props.handleToCart(props.product)} className='btn-regular'>{cartIcon}Add to Cart</button>
         </div>
     </div>
